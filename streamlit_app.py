a86a3216439d8cc5aaa1603cc64afcda4446314c
AkintondeAbbas <76406581+AkintondeAbbas@users.noreply.github.com>
2025-01-09 06:10:17 +0000

    contains code updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,17 @@
 import streamlit as st
 from openai import OpenAI
 
+st.sidebar.title("TUME AI")
+st.sidebar.title("TUME AI")
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+
+
 # Show title and description.
-st.title("💬 Chatbot")
+st.title("Energy Markets Data Explorer")
 st.write(
     "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
